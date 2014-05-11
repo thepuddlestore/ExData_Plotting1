fe2680a740be5dec4be32c7e5336e6a2d447331d
thepuddlestore <robbywest@me.com>
2014-05-11 13:24:06 -0400

    Plots again

--- plot3.R
+++ plot3.R
@@ -5,9 +5,9 @@ data$DateTime <- strptime(paste(data$Date, data$Time), format = "%d/%m/%Y %H:%M:
 ## Plot 3
 png(filename = "plot3.png", width = 480, height = 480, units = "px")
 with(data, {
-  plot(data$DateTime, data$Sub_metering_1, type = 'l', xlab = "", ylab = "Energy sub metering")
-  lines(data$DateTime, data$Sub_metering_2, type = 'l', xlab = "", col = "red")
-  lines(data$DateTime, data$Sub_metering_3, type = 'l', xlab = "", col = "blue")
-  legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = c(1, 1, 1), col = c("black", "red", "blue"))
+        plot(data$DateTime, data$Sub_metering_1, type = 'l', xlab = "", ylab = "Energy sub metering")
+        lines(data$DateTime, data$Sub_metering_2, type = 'l', xlab = "", col = "red")
+        lines(data$DateTime, data$Sub_metering_3, type = 'l', xlab = "", col = "blue")
+        legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = c(1, 1, 1), col = c("black", "red", "blue"))
 })
 dev.off()
